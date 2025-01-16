97353726a2fd7bed555ffcdbdc8cc511c3f81590
BugHunter-X <askzenai@gmail.com>
2025-01-16 13:58:06 +0100

    Add bugSolution.ts file

---
+++ bugSolution.ts
@@ -0,0 +1,9 @@
+function add(a: number, b: number): number {
+  if (typeof a !== 'number' || typeof b !== 'number') {
+    throw new Error('Both parameters must be numbers');
+  }
+  return a + b;
+}
+
+const result = add("1", 2); //Throws runtime error
+console.log(result); 
